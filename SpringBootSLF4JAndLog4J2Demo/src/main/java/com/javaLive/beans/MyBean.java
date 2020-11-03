44723758943cc10619202ab23a3f9a66a925f378
Administrator <Administrator@DELL>
2020-11-03 20:37:08 +0530

    Project file is added.

--- SpringBootSLF4JAndLog4J2Demo/src/main/java/com/javaLive/beans/MyBean.java
+++ SpringBootSLF4JAndLog4J2Demo/src/main/java/com/javaLive/beans/MyBean.java
@@ -9,6 +9,10 @@ public class MyBean {
     Logger logger = LoggerFactory.getLogger(MyBean.class);
 
     public void doSomething() {
-        logger.info("This is Info level log message from Mybean component.");
+        logger.info("==========This is Info level log message from Mybean component.================");
+        logger.trace("=========This is trace level log message from Mybean component.===============");
+        logger.debug("=========This is debug level log message from Mybean component.===============");
+        logger.warn("==========This is warn level log messagefrom Mybean component.=================");
+        logger.error("=========This is error level log messagefrom Mybean component.================");
     }
 }
